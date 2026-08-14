b7af7e714951be8bcb5837972140463f15751b12
0xsyung <93363780+0xsyung@users.noreply.github.com>
2026-08-14 10:54:31 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').Next.Config} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // NOTE: If your repository name is NOT '<username>.github.io', 
+  // uncomment the lines below and replace 'YOUR_REPO_NAME' with your actual GitHub repository name:
+  // basePath: '/YOUR_REPO_NAME',
+  // assetPrefix: '/YOUR_REPO_NAME/',
 };
 
 export default nextConfig;
+
